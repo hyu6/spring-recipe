e6623bdcbc62584676453d161fabcda1fd6a82f6
Haoran Yu <haoranyu6@gmail.com>
2020-04-10 06:45:33 -0700

    create UnitOfMeasure entity; create relationship between Ingredient and UnitOfMeasure

--- src/main/java/dev/hyux/springrecipe/domain/Ingredient.java
+++ src/main/java/dev/hyux/springrecipe/domain/Ingredient.java
@@ -12,6 +12,9 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
+
     @ManyToOne
     private Recipe recipe;
 
@@ -37,6 +40,14 @@ public class Ingredient {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
     }
 
     public Recipe getRecipe() {
