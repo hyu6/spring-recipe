77a38e593f3da0f3e63f1b666dd0e5326eb35cb0
Haoran Yu <haoranyu6@gmail.com>
2020-04-29 09:45:53 -0700

    refactor Recipe and Ingredient

--- src/main/java/dev/hyux/springrecipe/controllers/IndexController.java
+++ src/main/java/dev/hyux/springrecipe/controllers/IndexController.java
@@ -15,7 +15,6 @@ public class IndexController {
     }
 
     @RequestMapping({"", "/", "/index"})
-
     public String getIndexPage(Model model) {
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
