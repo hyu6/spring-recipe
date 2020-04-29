1c6228cbcb079a016378f54d56fe7f1073998a6b
Haoran Yu <haoranyu6@gmail.com>
2020-04-29 09:15:42 -0700

    create RecipeService; create default recipes on bootstrap

--- src/main/java/dev/hyux/springrecipe/controllers/IndexController.java
+++ src/main/java/dev/hyux/springrecipe/controllers/IndexController.java
@@ -1,35 +1,23 @@
 package dev.hyux.springrecipe.controllers;
 
-import dev.hyux.springrecipe.domain.Category;
-import dev.hyux.springrecipe.domain.UnitOfMeasure;
-import dev.hyux.springrecipe.repositories.CategoryRepository;
-import dev.hyux.springrecipe.repositories.UnitOfMeasureRepository;
+import dev.hyux.springrecipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
 
-    public String getIndexPage() {
-
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-
-        System.out.println("Cat id: " + categoryOptional.get().getId());
-        System.out.println("UOM id: " + unitOfMeasureOptional.get().getId());
-
+    public String getIndexPage(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
