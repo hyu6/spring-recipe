abfd467fb578c1bbce864ab10df30b5b371da37a
Haoran Yu <haoranyu6@gmail.com>
2020-05-01 06:42:49 -0700

    refactor codes with Project Lombok

--- src/main/java/dev/hyux/springrecipe/bootstrap/RecipeBootstrap.java
+++ src/main/java/dev/hyux/springrecipe/bootstrap/RecipeBootstrap.java
@@ -4,15 +4,18 @@ import dev.hyux.springrecipe.domain.*;
 import dev.hyux.springrecipe.repositories.CategoryRepository;
 import dev.hyux.springrecipe.repositories.RecipeRepository;
 import dev.hyux.springrecipe.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -27,8 +30,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         recipeRepository.saveAll(getRecipes());
+        log.debug("Loading Bootstrap Data");
     }
 
     private List<Recipe> getRecipes() {
